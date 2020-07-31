d9cae4511f02d8645616a49b0c769efa7fc09fd8
blacchoy@gmail.com <blacchoy@gmail.com>
2020-07-31 15:07:48 -0700

    add views and controller for Create Client

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -21,5 +21,20 @@ namespace HairSalon.Controllers
       List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
       return View(model);
     }
+
+    [HttpGet, ActionName("Create")]
+    public ActionResult Create(int id)
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+
+    [HttpPost, ActionName("Create")]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 }
 }
