280006758058af7d71fe6a667dae8c603cb1ecc5
blacchoy@gmail.com <blacchoy@gmail.com>
2020-07-31 13:05:58 -0700

    add view and controller for stylist index

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -6,13 +6,14 @@ namespace HairSalon.Models
 {
   public class Stylist
   {
-    public int SalonId { get; set; }
-    public string Name { get; set; }
-    public string ServicesOffered { get; set; }
-    public virtual ICollection<Client> Clients { get; set; }
-    public Stylist()
+     public Stylist()
     {
       this.Clients = new HashSet<Client>();
     }
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public string ServicesOffered { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+ 
   }
 } 
